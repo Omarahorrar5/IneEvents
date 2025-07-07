a0939a63c81b5f5503a811b6b00dbcc32919f1f5
omarahorrar <omarahorar@gmail.com>
2025-07-07 12:13:38 +0100

    adding event page structure

---
+++ IneClient/src/pages/EventPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="h-screen bg-surface m-0 p-0 overflow-x-hidden">
+        <Navbar />
+        <div>
+            <Photo />
+
+            <div>
+                <Infos />
+                <Description />
+                <RegisterButton />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Navbar from '../components/Event/Navbar.vue';
+import Photo from '../components/Event/Photo.vue';
+import Infos from '../components/Event/Infos.vue';
+import Description from '../components/Event/Description.vue';
+import RegisterButton from '../components/Event/RegisterButton.vue';
+</script>
