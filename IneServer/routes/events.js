29a75d8cd1ee6f7b06b3df14037660d29c6aedd2
omarahorrar <omarahorar@gmail.com>
2025-07-14 10:58:30 +0100

    adding delete event endpoint

--- IneServer/routes/events.js
+++ IneServer/routes/events.js
@@ -54,8 +54,12 @@ router.post('/events/update/:id', async (req, res) => {
         const {title, description, image_path, type, school, city, date, organizer} = req.body
 
         const result = await db.query(`update events set title = $1, description = $2, image_path = $3, type = $4, school = $5,
-            city = $6, date = $7, organizer = $8 where id = $9 returning *`, 
+            city = $6, date = $7, organizer = $8 where id = $9`, 
             [title, description, image_path, type, school, city, date, organizer, id])
+        
+        if (result.rowCount === 0) {
+            return res.status(404).json({ message: 'Event not found or not updated' });
+        }
 
         res.status(200).json({ message: 'Event updated successfully', event: result.rows[0] });
     }
@@ -64,5 +68,24 @@ router.post('/events/update/:id', async (req, res) => {
         res.status(500).json({ message: 'Could not update event', error: err.message });
     }
 })
+
+router.post('/events/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await db.query(`DELETE FROM events WHERE id = $1`, [id]);
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ message: 'Event not found or already deleted' });
+        }
+
+        res.status(200).json({ message: 'Event deleted successfully' });
+    }
+    
+    catch (err) {
+        res.status(500).json({ message: 'Could not delete event', error: err.message });
+    }
+});
+
 
 export default router
