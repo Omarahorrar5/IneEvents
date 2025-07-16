05e58cfbf8670619d60a1b4d706680c64ff92a0d
omarahorrar <omarahorar@gmail.com>
2025-07-16 09:58:54 +0100

    updating contact links

--- IneClient/src/components/Landing/Navbar.vue
+++ IneClient/src/components/Landing/Navbar.vue
@@ -1,11 +1,12 @@
 <template>
   <nav class="flex justify-between items-center px-6 h-16 bg-surface shadow-md">
 
-    <div class="flex items-center ml-2">
+    <div class="flex items-center">
       <img src="../../assets/logo.png" alt="IneEvents Logo" class="h-7 w-auto" />
     </div>
 
-    <ul class="flex mr-4 space-x-6 text-textMain font-medium text-base">
+    <ul class="flex space-x-6 text-textMain font-medium text-base">
+      <li><a href="#home" @click="scrollToAbout" class="hover:text-primary transition cursor-pointer">Home</a></li>
       <li><a href="#about" @click="scrollToAbout" class="hover:text-primary transition cursor-pointer">About</a></li>
     </ul>
   </nav>
