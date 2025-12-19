4ad9d4e2e7870ca347ed5d981e836bda1809f67f
Omarahorrar5 <omarahorar@gmail.com>
2025-12-19 17:34:10 +0100

    fix a space typo in registered events page

--- IneClient/src/pages/RegisteredPage.vue
+++ IneClient/src/pages/RegisteredPage.vue
@@ -30,7 +30,7 @@
                         <svg class="w-6 h-6 text-blue-500" fill="currentColor" viewBox="0 0 24 24">
                             <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/>
                         </svg>
-                        {{ registeredEvents.length }} Registered Event {{ registeredEvents.length !== 1 ? 's' : '' }}
+                        {{ registeredEvents.length }} Registered Event{{ registeredEvents.length !== 1 ? 's' : '' }}
                     </h2>
                     <div class="flex gap-2">
                         <button 
